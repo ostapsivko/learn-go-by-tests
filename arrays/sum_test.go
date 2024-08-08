f684368e392f3c560c0abfa09c9f68d7d2148f21
ostapsivko <o-stap@hotmail.com>
2024-08-09 00:58:47 +0300

    Arrays and TDD pt2

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,12 +5,14 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 items", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("git %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("git %d want %d", got, want)
+		}
+	})
 }

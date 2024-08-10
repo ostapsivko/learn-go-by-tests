b528fc93e10896aeaec4c8c54becd88b65a6e624
ostapsivko <o-stap@hotmail.com>
2024-08-10 17:46:55 +0300

    DI in Go

---
+++ writer/writer_test.go
@@ -0,0 +1,18 @@
+package writer
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}

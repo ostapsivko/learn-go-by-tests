018446b3959124317a6a0f97dae4750791cf2982
ostapsivko <o-stap@hotmail.com>
2024-08-13 22:05:32 +0300

    Add improved concurrency handling with Mutex

--- sync/counter.go
+++ sync/counter.go
@@ -1,10 +1,19 @@
 package counter
 
+import "sync"
+
 type Counter struct {
 	count int
+	mu    sync.Mutex
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.count++
 }
 

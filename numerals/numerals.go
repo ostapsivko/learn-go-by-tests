58b533772d2d23ecd7b8fe7fe4461b0d9abbf8c7
ostapsivko <o-stap@hotmail.com>
2024-08-15 23:07:04 +0300

    Add logic refactor and handle cases up to 8

--- numerals/numerals.go
+++ numerals/numerals.go
@@ -5,13 +5,18 @@ import "strings"
 func ConvertToRoman(value int) string {
 	var result strings.Builder
 
-	for i := value; i > 0; i-- {
-		if value == 4 {
+	for value > 0 {
+		switch {
+		case value > 4:
+			result.WriteString("V")
+			value -= 5
+		case value > 3:
 			result.WriteString("IV")
-			break
+			value -= 4
+		default:
+			result.WriteString("I")
+			value--
 		}
-
-		result.WriteString("I")
 	}
 
 	return result.String()
--- numerals/numerals_test.go
+++ numerals/numerals_test.go
@@ -12,6 +12,10 @@ func TestRomanNumerals(t *testing.T) {
 		{"2 to II", 2, "II"},
 		{"3 to III", 3, "III"},
 		{"4 to IV", 4, "IV"},
+		{"5 to V", 5, "V"},
+		{"6 to VI", 6, "VI"},
+		{"7 to VII", 7, "VII"},
+		{"8 to VIII", 8, "VIII"},
 	}
 
 	for _, test := range cases {

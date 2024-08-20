4055b74baed6cc25345316076886061e53f2c306
ostapsivko <o-stap@hotmail.com>
2024-08-20 21:58:33 +0300

    Add calculation and get acceptance test to pass

--- clock/clock.go
+++ clock/clock.go
@@ -5,14 +5,31 @@ import (
 	"time"
 )
 
+const (
+	secondHandLength = 90
+	clockCenterX     = 150
+	clockCenterY     = 150
+)
+
 type Point struct {
 	X, Y float64
 }
 
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+// represented as a Point.
 func SecondHand(date time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(date)
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} //scale
+	p = Point{p.X, -p.Y}                                      //flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         //translate
+	return p
 }
 
 func secondsInRadians(t time.Time) float64 {
 	return math.Pi / (30 / float64(t.Second()))
+}
+
+func secondHandPoint(t time.Time) Point {
+	radians := secondsInRadians(t)
+	return Point{math.Sin(radians), math.Cos(radians)}
 }
--- clock/clock_acceptance_test.go
+++ clock/clock_acceptance_test.go
@@ -17,13 +17,13 @@ func TestSecondHandAtMidnight(t *testing.T) {
 	}
 }
 
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
 
-// 	want := clock.Point{X: 150, Y: 150 + 90}
-// 	got := clock.SecondHand(tm)
+	want := clock.Point{X: 150, Y: 150 + 90}
+	got := clock.SecondHand(tm)
 
-// 	if got != want {
-// 		t.Errorf("got %v, wanted %v", got, want)
-// 	}
-// }
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}

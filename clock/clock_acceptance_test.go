309dce0e5180545c459e3cb7ef70f2591a09b610
ostapsivko <o-stap@hotmail.com>
2024-08-24 00:25:57 +0300

    Add refactoring of tests

--- clock/clock_acceptance_test.go
+++ clock/clock_acceptance_test.go
@@ -10,49 +10,53 @@ import (
 
 type SVG struct {
 	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
 	Xmlns   string   `xml:"xmlns,attr"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
 	ViewBox string   `xml:"viewBox,attr"`
 	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
+	Circle  Circle   `xml:"circle"`
+	Line    []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
 }
 
 func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	b := bytes.Buffer{}
-	clock.SVGWriter(&b, tm)
-
-	svg := SVG{}
-
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	x2 := "150.000"
-	y2 := "60.000"
-
-	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
-			return
-		}
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{simpleTime(0, 0, 0), Line{150, 150, 150, 60}},
+		{simpleTime(0, 0, 0), Line{150, 150, 150, 240}},
 	}
 
-	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+	for _, test := range cases {
+		t.Run(testName(test.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			clock.SVGWriter(&b, test.time)
+
+			svg := SVG{}
+
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			want := Line{150, 150, 150, 60}
+
+			if !containsLine(want, svg.Line) {
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %v", want, svg.Line)
+			}
+		})
+	}
 }
 
 func TestSecondHandAtMidnight(t *testing.T) {
@@ -75,4 +79,22 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 	if got != want {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(1337, time.January, 1, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+func containsLine(l Line, lines []Line) bool {
+	for _, line := range lines {
+		if line == l {
+			return true
+		}
+	}
+
+	return false
 }

abe2eff0a3970afff7dec5383592e8abfc2b26dc
ostapsivko <o-stap@hotmail.com>
2024-08-24 00:55:12 +0300

    Add minute hand and small refactor

--- clock/clock_acceptance_test.go
+++ clock/clock_acceptance_test.go
@@ -57,33 +57,33 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 	}
 }
 
-// func TestSVGWriterMinuteHand(t *testing.T) {
-// 	cases := []struct {
-// 		time time.Time
-// 		line Line
-// 	}{
-// 		{
-// 			simpleTime(0, 0, 0),
-// 			Line{150, 150, 150, 70},
-// 		},
-// 	}
+func TestSVGWriterMinuteHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 70},
+		},
+	}
 
-// 	for _, test := range cases {
-// 		t.Run(testName(test.time), func(t *testing.T) {
-// 			b := bytes.Buffer{}
+	for _, test := range cases {
+		t.Run(testName(test.time), func(t *testing.T) {
+			b := bytes.Buffer{}
 
-// 			clock.SVGWriter(&b, test.time)
+			clock.SVGWriter(&b, test.time)
 
-// 			svg := SVG{}
+			svg := SVG{}
 
-// 			xml.Unmarshal(b.Bytes(), &svg)
+			xml.Unmarshal(b.Bytes(), &svg)
 
-// 			if !containsLine(test.line, svg.Line) {
-// 				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %v", test.line, svg.Line)
-// 			}
-// 		})
-// 	}
-// }
+			if !containsLine(test.line, svg.Line) {
+				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %v", test.line, svg.Line)
+			}
+		})
+	}
+}
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)

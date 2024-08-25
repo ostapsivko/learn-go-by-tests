d615c86bed3f101f8836d192a67e218411a76b14
ostapsivko <o-stap@hotmail.com>
2024-08-25 10:52:16 +0300

    Add error handling

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -7,12 +7,17 @@ import (
 type Post struct {
 }
 
-func NewBlogPostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func NewBlogPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+
+	if err != nil {
+		return nil, err
+	}
+
 	var posts []Post
 	for range dir {
 		posts = append(posts, Post{})
 	}
 
-	return posts
+	return posts, nil
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -2,19 +2,42 @@ package blogposts_test
 
 import (
 	"blogposts"
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 )
 
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail...")
+}
+
 func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
-	}
+	t.Run("reading all files successfully", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte("hi")},
+			"hello-world2.md": {Data: []byte("hola")},
+		}
 
-	posts := blogposts.NewBlogPostsFromFS(fs)
+		posts, err := blogposts.NewBlogPostsFromFS(fs)
 
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, want %d", len(posts), len(fs))
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(posts) != len(fs) {
+			t.Errorf("got %d posts, want %d", len(posts), len(fs))
+		}
+	})
+
+	t.Run("reading with failing stub", func(t *testing.T) {
+		_, err := blogposts.NewBlogPostsFromFS(StubFailingFS{})
+
+		if err == nil {
+			t.Fatal("expected an error, but got none")
+		}
+	})
 }

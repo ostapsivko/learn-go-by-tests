2baedbc89282ccc382fdb24897c8ccf70c84b41b
ostapsivko <o-stap@hotmail.com>
2024-08-31 18:59:34 +0300

    Change asserts to generic version

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -6,19 +6,24 @@ func TestAssertFunctions(t *testing.T) {
 	t.Run("asserting on integers", func(t *testing.T) {
 		AssertEqual(t, 1, 1)
 		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
 		AssertEqual(t, "hello", "hello")
 		AssertNotEqual(t, "hello", "bye")
 	})
+
+	// AssertEqual(t, "1", 1)
 }
 
-func AssertEqual(t *testing.T, got, want any) {
+func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
 
-func AssertNotEqual(t *testing.T, got, want any) {
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got == want {
 		t.Errorf("did not want %+v", got)

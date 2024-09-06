2b8dca79065b9aefc7b3e834760d63d9fdeb2d2b
ostapsivko <o-stap@hotmail.com>
2024-09-06 22:43:40 +0300

    Change Greet to accept a name

--- go-specs-greet/handler.go
+++ go-specs-greet/handler.go
@@ -6,5 +6,6 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	name := r.URL.Query().Get("name")
+	fmt.Fprint(w, Greet(name))
 }
--- go-specs-greet/specifications/greet.go
+++ go-specs-greet/specifications/greet.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Greeter interface {
-	Greet() (string, error)
+	Greet(name string) (string, error)
 }
 
 func GreetSpecification(t testing.TB, greeter Greeter) {
-	got, err := greeter.Greet()
+	got, err := greeter.Greet("Azdab")
 
 	assert.NoError(t, err)
-	assert.Equal(t, got, "Hello, World!")
+	assert.Equal(t, got, "Hello, Azdab!")
 }

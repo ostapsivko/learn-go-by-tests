ee787f7032d4d8ce342a76611e0b87da3cdaa6c1
ostapsivko <o-stap@hotmail.com>
2024-09-15 11:14:57 +0300

    Add stub of players score store

--- http-server/server.go
+++ http-server/server.go
@@ -3,8 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+
+	return ""
 }

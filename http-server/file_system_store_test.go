c9de9274319fd55f112c25825e30c130ee358fee
ostapsivko <o-stap@hotmail.com>
2024-09-19 00:26:59 +0300

    Add player score retrieval

--- http-server/file_system_store_test.go
+++ http-server/file_system_store_test.go
@@ -25,4 +25,25 @@ func TestFileSystemStore(t *testing.T) {
 		got = store.GetLeague()
 		assertLeague(t, got, want)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		database := strings.NewReader(`[
+		{"Name": "Azdab", "Score": 10},
+		{"Name": "Andrii", "Score": 33}]`)
+
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetPlayerScore("Azdab")
+
+		want := 10
+		assertScoreEquals(t, got, want)
+	})
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
 }
